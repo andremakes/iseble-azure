ec43a320e6f1e23fd711dc7285358dfd6fc97c83
Andre <codeyonet@gmail.com>
2025-07-28 23:55:16 -0700

    Readme Update

--- examples/env_example.sh
+++ examples/env_example.sh
@@ -10,7 +10,7 @@ export AZURE_SUBSCRIPTION_ID="your-azure-subscription-id"
 export AZURE_TENANT="your-azure-tenant-id"
 
 # ISE Configuration
-export ISE_PASSWORD="ISEisC00LISE"
+export ISE_PASSWORD="ISEisC00L"
 export ISE_DOMAIN="your-domain.com"
 
 # SSH Configuration (optional - will be generated if not set)
@@ -24,4 +24,4 @@ export LOCAL_GATEWAY_IP="10.0.0.1"
 # 1. Copy to env.sh: cp examples/env_example.sh env.sh
 # 2. Edit env.sh with your values
 # 3. Source the file: source env.sh
-# 4. Run deployment: ansible-playbook playbooks/01-create-infrastructure.yml 
+# 4. Run deployment: ansible-playbook playbooks/01-create-infrastructure.yml
